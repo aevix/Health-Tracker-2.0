484ed8f3095a5f282a61ac5f86a1c14dfe4d2d43
aevix <55609333+aevix@users.noreply.github.com>
2020-10-19 01:25:25 -0500

    add eslintrc file

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,6 @@
   <amplify-authenticator>
     <div id="app">
       <h1>Todo App</h1>
-      <h2>testing</h2>
       <input type="text" v-model="name" placeholder="Todo name" />
       <input type="text" v-model="description" placeholder="Todo description" />
       <button v-on:click="createTodo">Create Todo</button>
