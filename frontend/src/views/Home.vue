494bca6830b8b99f467e0e6f3b959c613862b1a4
aevix <55609333+aevix@users.noreply.github.com>
2020-10-18 23:01:33 -0500

    run format with prettier plugin

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <amplify-authenticator>
-      <div id="app">
-        <h1>Todo App</h1>
-        <input type="text" v-model="name" placeholder="Todo name" />
-        <input type="text" v-model="description" placeholder="Todo description" />
-        <button v-on:click="createTodo">Create Todo</button>
-        <div v-for="item in todos" :key="item.id">
-          <h3>{{ item.name }}</h3>
-          <p>{{ item.description }}</p>
-        </div>
+    <div id="app">
+      <h1>Todo App</h1>
+      <input type="text" v-model="name" placeholder="Todo name" />
+      <input type="text" v-model="description" placeholder="Todo description" />
+      <button v-on:click="createTodo">Create Todo</button>
+      <div v-for="item in todos" :key="item.id">
+        <h3>{{ item.name }}</h3>
+        <p>{{ item.description }}</p>
       </div>
+    </div>
     <amplify-sign-out></amplify-sign-out>
   </amplify-authenticator>
 </template>
