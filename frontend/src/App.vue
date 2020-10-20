960a72e019bfa612ed52c46ebfcef73351f8f3a9
aevix <55609333+aevix@users.noreply.github.com>
2020-10-20 03:39:03 -0500

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,8 @@
   <amplify-authenticator>
     <div id="app">
       <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
+        <router-link to="/"> Home </router-link> |
+        <router-link to="/about"> About </router-link>
         <div id="button-container">
           <amplify-sign-out></amplify-sign-out>
         </div>
