0fb7581800ad6bde0b4fef5a434ac8fee34c9c12
aevix <55609333+aevix@users.noreply.github.com>
2020-10-21 01:24:06 -0500

    center login screen

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <amplify-authenticator>
-    <div id="app">
-      <div id="nav">
-        <router-link to="/"> Home </router-link> |
-        <router-link to="/about"> About </router-link>
-        <div id="button-container">
-          <amplify-sign-out></amplify-sign-out>
+  <div class="app-container">
+    <amplify-authenticator>
+      <div id="app">
+        <div id="nav">
+          <router-link to="/"> Home </router-link> |
+          <router-link to="/about"> About </router-link>
+          <div id="button-container">
+            <amplify-sign-out></amplify-sign-out>
+          </div>
         </div>
+        <router-view />
       </div>
-      <router-view />
-    </div>
-  </amplify-authenticator>
+    </amplify-authenticator>
+  </div>
 </template>
 
 <script>
@@ -26,24 +28,40 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  text-align: center;
+}
+
+.app-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  box-shadow: none;
+}
+
+amplify-authenticator {
+  box-shadow: none;
 }
 
 #nav {
   padding: 30px;
-  display: inline-block;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 
   a {
     font-weight: bold;
     color: #2c3e50;
+    margin: 10px;
 
     &.router-link-exact-active {
       color: #42b983;
     }
   }
-}
 
-#button-container {
-  width: 50px;
+  #button-container {
+    width: 50px;
+    margin: 10px;
+  }
 }
 </style>
